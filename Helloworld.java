7095d1544defbae6a67b289b16c9f16b6e6a9fcb
imrankhan4969 <imran.m.pathan@gmail.com>
2019-04-28 11:51:51 +0000

    Added my first file

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+/* This is a simple Java program.
+   FileName : "HelloWorld.java". */
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, line-1");
+	System.out.println("Hello, line-2");
+	System.out.println("Hello, Line-3");
+    }
+}
+
